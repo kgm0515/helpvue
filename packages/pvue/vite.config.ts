a3271bbf34e0dda3652b440b89f030cd112e3e25
匡光淼 <guangmiao.kuang@qizhidao.com>
2023-03-29 17:34:28 +0800

    feat: 开发中...

--- packages/pvue/vite.config.ts
+++ packages/pvue/vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: [
-        { format: 'umd', dir: './dist', entryFileNames: 'index.umd.js', name: 'Pvue', globals: { vue: 'Vue' } },
+        { format: 'umd', dir: './dist', entryFileNames: 'index.umd.js', name: 'HelpPvue', globals: { vue: 'Vue' } },
         { format: 'es', dir: './dist/es', entryFileNames: '[name].js', preserveModules: true, preserveModulesRoot: 'src' },
         { format: 'cjs', dir: './dist', entryFileNames: 'index.cjs.js', globals: { vue: 'Vue' } },
-        { format: 'iife', dir: './dist', entryFileNames: 'index.iife.js', name: 'Pvue', globals: { vue: 'Vue' } }
+        { format: 'iife', dir: './dist', entryFileNames: 'index.iife.js', name: 'HelpPvue', globals: { vue: 'Vue' } }
       ]
     }
   }
