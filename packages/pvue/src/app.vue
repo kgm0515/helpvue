cb77ac64f99d517a08aad6c9d94339a058397f34
匡光淼 <guangmiao.kuang@qizhidao.com>
2023-03-30 10:25:45 +0800

    feat: 开发中...

---
+++ packages/pvue/src/app.vue
@@ -0,0 +1,13 @@
+<template></template>
+<script lang="ts">
+  import { defineComponent, PropType, computed } from 'vue'
+  export default defineComponent({
+    name: 'app',
+    setup() {
+      return {}
+    }
+  })
+</script>
+<style lang="less">
+  @import url('../style/index.less');
+</style>
--- packages/pvue/src/index.ts
+++ packages/pvue/src/index.ts
@@ -2,7 +2,8 @@ import type { App } from 'vue'
 import * as components from './components'
 export * from './components'
 import { IAppConfig } from './helper'
-
+import RootComp from './app.vue'
+console.log(RootComp.name)
 export function install(app: App, config?: IAppConfig) {
   for (const keyName in components) {
     const Comp = (components as any)[keyName]
