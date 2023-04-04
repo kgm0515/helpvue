70a052b32b191d9c72c4bf8f240075fd6f2efa0a
匡光淼 <guangmiao.kuang@qizhidao.com>
2023-04-04 20:02:46 +0800

    feat: 测试vite

--- example/testvite/src/app.vue
+++ example/testvite/src/app.vue
@@ -1,26 +1,20 @@
 <template>
-  <div>
-    <h1>{{ count }}</h1>
+  <div :class="styleObj.root">
+    <h1 :class="styleObj['root-title']">{{ count }}</h1>
     <button @click="getCount">click</button>
   </div>
 </template>
 <script>
   import { defineComponent, ref } from 'vue'
+  import styleObj from './assets/css/style.module.less'
 
   export default defineComponent({
     setup() {
+      console.log(styleObj)
       const count = ref(0)
       const getCount = () => count.value++
-      return { count, getCount }
+      return { styleObj, count, getCount }
     }
   })
 </script>
-<style lang="less" scoped>
-  div {
-    color: red;
-    button {
-      color: inherit;
-      font-size: 50px;
-    }
-  }
-</style>
+<style lang="less" scoped></style>
--- example/testvite/src/index.js
+++ example/testvite/src/index.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './app.vue'
-
+import './assets/css/base.css'
 // import * as lodash from 'lodash-es'
 console.log('环境变量: ', import.meta.env)
 
